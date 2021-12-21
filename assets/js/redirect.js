0524a88b619a5fbdc85e5945e56aaaf7c7ef0671
Flo <92175215+mopsfl@users.noreply.github.com>
2021-12-21 19:44:15 +0100

    Update redirect.js
    Fixed redirect query detect

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,39 +1,65 @@
 var redirects = [
     youtube = "https://www.youtube.com/channel/UCFkA8OJnMdApJuIfOU42Qzg",
-    github = "https://www.youtube.com/channel/UCFkA8OJnMdApJuIfOU42Qzg",
+    github = "https://github.com/mopsfl",
     twitter = "https://twitter.com/tonimahoni854",
-    home = "https://mopsfl.github.io"
+    home = "../",
 ]
 
-function redirect(id) {
-    if (id == "youtube") {
-        setTimeout(() => {
-            console.warn("[mopsfl.github.io]: Redirected to youtube")
-            window.location = redirects[0]
-        }, 10);
-    } else {
-        if (id == "github") {
-            setTimeout(() => {
-                console.warn("[mopsfl.github.io]: Redirected to github")
-                window.location = redirects[1]
-            }, 10);
-        } else {
-            if (id == "twitter") {
-                setTimeout(() => {
-                    console.warn("[mopsfl.github.io]: Redirected to twitter")
-                    window.location = redirects[2]
-                }, 10);
-            } else {
-                console.warn("[mopsfl.github.io]: Invalid redirect id")
-                window.location = redirects[3]
-                return
-            }
+var parseQueryString = function() {
+
+    var str = window.location.search;
+    var objURL = {};
+
+    str.replace(
+        new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+        function($0, $1, $2, $3) {
+            objURL[$1] = $3;
         }
+    );
+    return objURL;
+};
+var params = parseQueryString();
+
+
+function redirect(id) {
+    if (id != "" && id != undefined && !isNaN(id)) {
+        try {
+            window.open(redirects[id], "_self")
+            console.log("Redirected to [" + redirects[id] + "]")
+        } catch (error) {
+            console.warn("Cannot redirect [" + error + "]")
+        }
+    } else {
+        console.warn("Invalid id [" + id + "]")
     }
 }
 
 window.onload = function() {
     setTimeout(() => {
-        redirect(redirect(window.location.search.split('id=')[1]))
-    }, 1000);
+        if (window.location.search != "") {
+            if (params["r"] != undefined && params["r"]) {
+                if (params["r"] == "youtube") {
+                    console.log("Redirect to " + params["r"])
+                    return redirect(0)
+                } else if (params["r"] == "github") {
+                    console.log("Redirect to " + params["r"])
+                    return redirect(1)
+                } else if (params["r"] == "twitter") {
+                    console.log("Redirect to " + params["r"])
+                    return redirect(2)
+                } else if (params["r"] == "home") {
+                    return redirect(3)
+                } else {
+                    console.warn("Invalid redirect id [" + params["r"] + "]")
+                    return redirect(3)
+                }
+            } else {
+                console.warn("Invalid redirect id [" + params["r"] + "]")
+                redirect(3)
+            }
+        } else {
+            console.warn("Invalid redirect format")
+            redirect(3)
+        }
+    }, 100)
 }
