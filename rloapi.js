af1bfd1b08b14418db4c6050259290ccdf8b631d
Flo <92175215+mopsfl@users.noreply.github.com>
2022-12-25 16:35:56 +0100

    Create rloapi.js
    api for rlo stats (node js)

---
+++ rloapi.js
@@ -0,0 +1,50 @@
+const fivem = require("fivem-server-stats")
+const express = require("express")
+const app = express()
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+})
+
+app.get('/api/rlo/players', (req, res) => {
+    try {
+        fivem.getPlayers("reallifeonline.cf:30120").then((data) => res.json(data))
+    } catch (error) {
+        res.json({ code: 500, message: "Internal Server Error" })
+        console.error(error)
+    }
+});
+
+app.get('/api/rlo/online', (req, res) => {
+    try {
+        fivem.getPlayerLength("reallifeonline.cf:30120").then((data) => res.json(data))
+    } catch (error) {
+        res.json({ code: 500, message: "Internal Server Error" })
+        console.error(error)
+    }
+});
+
+app.get('/api/rlo/info', (req, res) => {
+    try {
+        fivem.getInfo("reallifeonline.cf:30120").then((data) => res.json(data))
+    } catch (error) {
+        res.json({ code: 500, message: "Internal Server Error" })
+        console.error(error)
+    }
+});
+
+app.get('/api/rlo/dynamic', (req, res) => {
+    try {
+        fivem.getDynamic("reallifeonline.cf:30120").then((data) => res.json(data))
+    } catch (error) {
+        res.json({ code: 500, message: "Internal Server Error" })
+        console.error(error)
+    }
+});
+app.listen(3000, () => {
+    console.log('server started');
+});
