f84a0bf1e3378e3e2fa4c44620a7d5c721de5750
Flo <92175215+mopsfl@users.noreply.github.com>
2022-12-25 16:34:53 +0100

    Update rlo.js

--- static/js/rlo.js
+++ static/js/rlo.js
@@ -10,12 +10,59 @@ headers.append('Access-Control-Allow-Origin', '*');
 headers.append('Accept', "application/json")
 headers.append('Content-Type', "application/json")
 
-async function api() {
-    const data = await fetch(`http://reallifeonline.cf:30120/players.json`, { mode: "no-cors", headers: headers })
+async function getPlayers() {
+    const data = await fetch(`http://localhost:3000/api/rlo/players`)
     const json = await data.json()
     return json
 }
 
-api().then(res => {
-    console.log(res);
-})
+async function getDynamic() {
+    const data = await fetch(`http://localhost:3000/api/rlo/dynamic`)
+    const json = await data.json()
+    return json
+}
+
+async function getInfo() {
+    const data = await fetch(`http://localhost:3000/api/rlo/info`)
+    const json = await data.json()
+    return json
+}
+
+function qs(element) {
+    return document.querySelector(element)
+}
+
+async function load() {
+    qs("[data-players-list]").innerHTML = ""
+    const dynamic = await getDynamic()
+    const players = await getPlayers()
+    const info = await getInfo()
+    if (dynamic && dynamic.clients) {
+        qs("[data-plr-count]").innerText = dynamic.clients + "/" + dynamic.sv_maxclients
+    } else if (dynamic && dynamic.message) {
+        console.error(dynamic.error)
+        const plrElement = qs("[data-plrtemplate]").content.cloneNode(true).children[0]
+        plrElement.querySelector("[data-name]").innerText = "Error while requesting dynamic.json"
+        qs("[data-players-list]").appendChild(plrElement)
+        return
+    }
+    if (players) {
+        players.forEach(player => {
+            const plrElement = qs("[data-plrtemplate]").content.cloneNode(true).children[0]
+            plrElement.querySelector("[data-name]").innerText = `${player.name}`
+            plrElement.querySelector("[data-id]").innerText = `(${player.id})`
+            qs("[data-players-list]").appendChild(plrElement)
+        });
+    }
+    if (info) {
+        qs("[data-servername]").innerText = info.vars.sv_projectName
+    }
+}
+
+window.onload = () => {
+    load()
+
+    qs("[data-reload]").addEventListener("click", (e) => {
+        load()
+    })
+}
