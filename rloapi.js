9a92b317c5f64645af7093ff09f078ba357b5618
Flo <92175215+mopsfl@users.noreply.github.com>
2022-12-25 16:40:12 +0100

    Update rloapi.js

--- rloapi.js
+++ rloapi.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*);
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
